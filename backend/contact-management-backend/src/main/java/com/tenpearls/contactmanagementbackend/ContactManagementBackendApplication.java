84ebd77eb90ad0c2945fde05dee9f0de61f741c5
unknown <mmirabbutt@gmail.com>
2026-08-12 13:33:31 -0700

    Setup Spring Boot project with SQL Server database connection

---
+++ backend/contact-management-backend/src/main/java/com/tenpearls/contactmanagementbackend/ContactManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tenpearls.contactmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactManagementBackendApplication.class, args);
+    }
+
+}
---
+++ backend/contact-management-backend/src/test/java/com/tenpearls/contactmanagementbackend/ContactManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenpearls.contactmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
